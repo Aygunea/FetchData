d96ffd5d4b2377a7b9fd156fb40ba9538aa06cda
aygun aghalarova <agalarova.aygunn@gmail.com>
2024-02-27 00:20:34 +0400

    fetch data

--- script.js
+++ script.js
@@ -27,3 +27,43 @@ fetch("https://northwind.vercel.app/api/products")
   .catch((error) => {
     console.error("Fetch işlemi sırasında bir hata oluştu:", error);
   });
+// .https://api.tvmaze.com/shows
+
+const cards = document.getElementById("cards");
+
+fetch("https://api.tvmaze.com/shows")
+    .then(response => response.json())
+    .then(data => {
+      data.forEach(show => {
+        console.log(show);
+        const card = document.createElement('div');
+        card.classList.add('card');
+
+        const img = document.createElement('img');
+        img.src = show.image.medium
+        card.appendChild(img);
+
+        const title = document.createElement('h2');
+        title.textContent = show.name;
+        card.appendChild(title);
+
+        const language = document.createElement('p');
+        language.textContent = `Language: ${show.language}`;
+        card.appendChild(language);
+
+        const rating = document.createElement('p');
+        rating.textContent = `Rating: ${show.rating.average || 'N/A'}`;
+        card.appendChild(rating);
+
+        const genres = document.createElement('p');
+        genres.textContent = `Genres: ${show.genres.join(', ')}`;
+        card.appendChild(genres);
+
+        // Diğer özellikler buraya eklenebilir (örneğin, resim vb.)
+
+        cards.appendChild(card);
+      });
+    })
+    .catch(error => {
+      console.error('Bir hata oluştu:', error);
+    });
